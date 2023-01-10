1c669c3f2ba9e3e130bebbfba7dd98af5e5eade9
Daniel Kelly <me@danielkelly.io>
2023-01-10 17:17:43 -0600

    exercise 2 solution page

---
+++ src/completed/pages/exercise-2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ExerciseHeader n="2" />
+    <DynamicHeading level="2">Bye Bye Functional Components</DynamicHeading>
+  </div>
+</template>
+<script>
+import DynamicHeading from "../components/DynamicHeadingFunctional.vue";
+export default {
+  components: {
+    DynamicHeading,
+  },
+};
+</script>
