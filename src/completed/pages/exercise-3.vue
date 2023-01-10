7e5e669f8338f63980d873ad8290b27398b3b991
Daniel Kelly <me@danielkelly.io>
2023-01-10 17:20:58 -0600

    exercise 3 solution

---
+++ src/completed/pages/exercise-3.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ExerciseHeader n="3" />
+    <div>
+      <p>{{ allCaps(msg) }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello world",
+    };
+  },
+  methods: {
+    allCaps(value) {
+      return value.toUpperCase();
+    },
+  },
+};
+</script>
