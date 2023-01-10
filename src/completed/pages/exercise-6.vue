265d03f1ee6d271cee935e94f8664736802b938b
Daniel Kelly <me@danielkelly.io>
2023-01-10 17:31:28 -0600

    solution exercise 6

---
+++ src/completed/pages/exercise-6.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ExerciseHeader n="6" />
+    <DataSender />
+  </div>
+</template>
+
+<script>
+import DataSender from "../components/DataSender.vue";
+export default {
+  components: { DataSender },
+};
+</script>
