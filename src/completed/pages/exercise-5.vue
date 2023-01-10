cb6bcb9af9738715a079f118d303a380faaf7508
Daniel Kelly <me@danielkelly.io>
2023-01-10 17:27:21 -0600

    solution exercise 5

---
+++ src/completed/pages/exercise-5.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ExerciseHeader n="5" />
+    <!-- This is the component you'll create -->
+    <ColorPreview />
+  </div>
+</template>
+
+<script>
+// Change this import to your newly created component
+import ColorPreview from "@/completed/components/ColorPreview.vue";
+export default {
+  components: { ColorPreview },
+};
+</script>
---
+++ src/components/ColorPreview.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Create your ColorPreview component here</p>
+</template>
